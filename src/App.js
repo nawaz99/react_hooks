f4a0b992cb20bfa4a2d4444b1f63cf973ac335d6
Nawaz9191 <nawaz@itradesoft.com>
2023-06-19 16:42:19 +0530

    updated changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        useState Hooks example
+        React Hooks {count}
+        <button onClick={() => setCount((prev) => prev + 1)}>
+          Increment +
+        </button>
+        <button
+          onClick={() => {
+            if (count != 0) {
+              setCount((prev) => {
+                return prev - 1;
+              });
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Decrement -
+        </button>
+      </header>  
     </div>
   );
 }
